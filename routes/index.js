0bb321f12d025be450532e45c997a675f17f50a2
Ervin Gonzalez <83973082+Ervin001@users.noreply.github.com>
2023-10-27 00:25:18 -0500

    Changed all the var variables to const

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,8 @@ const messages = [
   },
 ];
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
